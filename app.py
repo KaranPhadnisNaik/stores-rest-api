5b2b631558af7e53f5e6917b12414c3219c5c86e
karanphadnisnaik <Karanphadnis1@gmail.com>
2017-08-30 23:28:32 -0700

    first commit - restapi for items,stores, and user authentication

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask
+from flask_restful import Api, reqparse
+from flask_jwt import JWT
+
+from security import authenticate,identity
+from resources.user import UserRegister
+from resources.item import Item, ItemList
+from resources.store import Store, StoreList
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.secret_key = 'karan'
+api = Api(app)
+
+# create all the tables unless the exist already
+@app.before_first_request
+def create_tables():
+    db.create_all()
+
+jwt = JWT(app, authenticate,identity) # creates a new endpoint /auth,
+                                      # sends username and password to authnetiate function
+
+api.add_resource(Store, '/store/<string:name>') # localhost:9000/Item/Rolf
+api.add_resource(Item, '/item/<string:name>') # localhost:9000/Item/Rolf
+api.add_resource(ItemList, '/items') # localhost:9000/Item/Rolf
+api.add_resource(StoreList, '/stores') # localhost:9000/Item/Rolf
+
+api.add_resource(UserRegister, '/register') # localhost:9000/register
+
+if __name__ == '__main__':
+    from db import db
+    db.init_app(app)
+    app.run(port=9000, debug=True)
